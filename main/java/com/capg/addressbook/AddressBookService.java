2640ea4d4ebbf7e7dc18493a15fe59822f8d47eb
Jay002 <jayesh2598@gmail.com>
2020-11-02 23:27:48 +0530

    Start: Welcome message added.

---
+++ main/java/com/capg/addressbook/AddressBookService.java
@@ -0,0 +1,8 @@
+package com.capg.addressbook;
+
+public class AddressBookService {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to address book database system.");
+	}
+}
